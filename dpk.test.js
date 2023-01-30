ad6b40932833e4bdc84b0cf8441a1577934f3303
adonisdev923 <adonisdev923@gmail.com>
2023-01-30 16:19:53 +0900

    Complete test

--- dpk.test.js
+++ dpk.test.js
@@ -4,5 +4,16 @@ describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+    console.log(trivialKey)
   });
+  it("Returns partition key when given a partition key", () => {
+    const trivialKey = deterministicPartitionKey({ partitionKey: "partitionKey" });
+    expect(trivialKey).toBe("partitionKey");
+    console.log(trivialKey)
+  })
+  it("Returns partition key", () => {
+    const trivialKey = deterministicPartitionKey({ randomKey: 'random partition key' });
+    expect(trivialKey.length).toBeGreaterThan(0)
+    console.log(trivialKey)
+  })
 });
